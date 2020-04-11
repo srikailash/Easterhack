3d23f04cdf07603cf7a85443d8b2d87900dcf5d7
Sri Kailash <srikailashukcs@gmail.com>
2020-04-11 15:05:28 +0200

    some integration

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var http = require("http");
 var fs = require("fs");
+//variables for the game
+var game_id;
+var player1_socket_id;
+var player2_socket_id;
+
 
 var app = express();
 var server = app.listen(3000);
@@ -12,8 +17,24 @@ app.use(express.static(__dirname + "/frontend"));
 //Apparently, socket.io is a http server by itself
 var io = require("socket.io").listen(server);
 
-io.on("connection", function (socket) {
+
+
+io.on('connection', function(socket) {
+
+	socket.on('gamestart', () => {
+		console.log('Got gamestart event from the clientside');
+		player1_socket_id = socket.id;
+		socket.emit('game_id', {message: '123', id: socket.id});
+	});
+
+	socket.on('joingame', (game_id) => {
+		console.log('Joining Game with socket_id ' + socket.id);
+		player2_socket_id = socket.id;
+		socket.emit('welcome', {message: "You've now joined the game", id: socket.id });
+	});
   //Using socket to communicate with the client
   socket.emit("welcome", { message: "Welcome!", id: socket.id });
   socket.on("i am client", console.log);
 });
+
+
