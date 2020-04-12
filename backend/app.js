558911b298127c702ad80a595cf1c5886285c2b7
Sri Kailash <srikailashukcs@gmail.com>
2020-04-12 08:30:28 +0200

    game_id to math.random() and fixed bad movement, todo: FE changes to hide buttons on click

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var http = require("http");
 var fs = require("fs");
+
 //variables for the game
+var player1 = [];
+var player2 = [];
 var game_id;
 var player1_socket_id;
 var player2_socket_id;
-var ball_x;
-var ball_y;
-var player1_y;
-var player2_y;
 
 var app = express();
 var server = app.listen(3000);
@@ -25,13 +24,22 @@ io.on('connection', function(socket) {
 	socket.on('gamestart', () => {
 		console.log('Got gamestart event from the clientside ' + socket.id);
 		player1_socket_id = socket.id;
-		socket.emit('game_id', {message: '123', id: socket.id});
+		game_id = Math.random();
+		player1.push({
+			key: game_id,
+			value: player1_socket_id
+		});
+		io.sockets.connected[player1_socket_id].emit('game_id', { message: game_id });
 	});
 
 	socket.on('joingame', (game_id) => {
 		console.log('Joining Game with socket_id ' + socket.id);
 		player2_socket_id = socket.id;
-		socket.emit('welcome', {message: "You've now joined the game", id: socket.id });
+		player2.push({
+			key: game_id,
+			value: player2_socket_id
+		});
+		io.sockets.connected[player2_socket_id].emit('welcome', { message: "You've now joined the game" });
 	});
 
 	socket.on('player_hit', function(data) {
@@ -49,8 +57,9 @@ io.on('connection', function(socket) {
 			console.log('moving position of player2');
 			io.sockets.connected[player2_socket_id].emit('player_move', { message: new_positions });
 		}
-		else if(typeof player1_socket_id !== 'undefined')
+		else if(socket.id === player2_socket_id && typeof player1_socket_id !== 'undefined')
 		{
+			//This if condition will go away as game is started only when both start
 			console.log('moving position of player1');
 			io.sockets.connected[player1_socket_id].emit('player_move', { message: new_positions });
 		}
