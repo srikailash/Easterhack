cbf0e55c15b6cbb2a20e22faa14a27e0259799df
Abhilash Gnan <abhilashgnan@gmail.com>
2020-04-13 10:15:53 +0200

    indent app.js

--- backend/app.js
+++ backend/app.js
@@ -16,61 +16,77 @@ app.use(express.static(__dirname + "/frontend"));
 //Apparently, socket.io is a http server by itself
 var io = require("socket.io").listen(server);
 
-io.on('connection', function(socket) {
+io.on("connection", function (socket) {
+  socket.on("gamestart", () => {
+    //console.log('Got gamestart event from the clientside ' + socket.id);
+    var player1_socket_id = socket.id;
+    game_id = Math.floor(Math.random() * 90000) + 10000;
+    player1[game_id] = player1_socket_id;
+    player2[game_id] = -1;
+    console.log("SENDING GAME_ID BACK TO PLAYER1 : " + game_id);
+    io.sockets.connected[player1_socket_id].emit("gamestart", {
+      message: game_id,
+    });
+  });
 
-	socket.on('gamestart', () => {
-		//console.log('Got gamestart event from the clientside ' + socket.id);
-		var player1_socket_id = socket.id;
-		game_id = Math.floor(Math.random()*90000) + 10000;
-		player1[game_id] = player1_socket_id;
-		player2[game_id] = -1;
-		console.log('SENDING GAME_ID BACK TO PLAYER1 : ' + game_id);
-		io.sockets.connected[player1_socket_id].emit('gamestart', { message: game_id });
-	});
+  socket.on("joingame", function (data) {
+    console.log(
+      "GAME ID REQUESTED TO JOIN : : :  " + data.gameId + " " + player2[game_id]
+    );
+    game_id = data.gameId;
+    var player2_socket_id = socket.id;
 
-	socket.on('joingame', function(data) {
-		console.log('GAME ID REQUESTED TO JOIN : : :  ' + data.gameId + ' ' + player2[game_id]);
-		game_id = data.gameId;
-		var player2_socket_id = socket.id;
+    if (player2[game_id] !== undefined) {
+      var player1_socket_id = player1[game_id];
+      //presense of game_id in player2 indicates a valid game_id
+      player2[game_id] = player2_socket_id;
+      io.sockets.connected[player2_socket_id].emit("welcome", {
+        message: "You've now joined the game",
+      });
+      io.sockets.connected[player1_socket_id].emit("actually_start", {
+        message: "Starting the Game " + game_id,
+      });
+      io.sockets.connected[player2_socket_id].emit("actually_start", {
+        message: "Starting the Game " + game_id,
+      });
+    } else {
+      io.sockets.connected[player2_socket.id].emit("invalid_game_id", {
+        message: "Game Id you've entered is not a valid one",
+      });
+    }
+  });
 
-		if(player2[game_id] !== undefined) {
-			var player1_socket_id = player1[game_id];
-			//presense of game_id in player2 indicates a valid game_id
-			player2[game_id] = player2_socket_id;
-			io.sockets.connected[player2_socket_id].emit('welcome', { message: "You've now joined the game" });
-			io.sockets.connected[player1_socket_id].emit('actually_start', { message: "Starting the Game " + game_id });
-			io.sockets.connected[player2_socket_id].emit('actually_start', { message: "Starting the Game " + game_id });
-		}
-		else {
-			io.sockets.connected[player2_socket.id].emit('invalid_game_id', { message: "Game Id you've entered is not a valid one"});
-		}
+  socket.on("player_hit", function (data) {
+    socket.broadcast.emit("move", { message: data });
+  });
 
-	});
+  socket.on("player_move", function (data) {
+    //console.log('Moving : ' + socket.id);
+    var new_positions = {
+      myPosition: data.otherPosition,
+      otherPosition: data.myPosition,
+    };
+    var game_id = data.game_id;
+    var player1_socket_id = player1[game_id];
+    var player2_socket_id = player2[game_id];
 
-	socket.on('player_hit', function(data) {
-		socket.broadcast.emit('move', { message: data });
-	});
-
-	socket.on('player_move', function(data) {
-		//console.log('Moving : ' + socket.id);
-		var new_positions = {
-			myPosition: data.otherPosition,
-			otherPosition: data.myPosition
-		}
-		var game_id = data.game_id;
-		var player1_socket_id = player1[game_id];
-		var player2_socket_id = player2[game_id];
-
-		if(socket.id === player1_socket_id && typeof player2_socket_id !== 'undefined') {
-		//	console.log('moving position of player2');
-			io.sockets.connected[player2_socket_id].emit('player_move', { message: new_positions });
-		}
-		else if(socket.id === player2_socket_id && typeof player1_socket_id !== 'undefined')
-		{
-			//This if condition will go away as game is started only when both start
-		//	console.log('moving position of player1');
-			io.sockets.connected[player1_socket_id].emit('player_move', { message: new_positions });
-		}
-	});
-
+    if (
+      socket.id === player1_socket_id &&
+      typeof player2_socket_id !== "undefined"
+    ) {
+      //	console.log('moving position of player2');
+      io.sockets.connected[player2_socket_id].emit("player_move", {
+        message: new_positions,
+      });
+    } else if (
+      socket.id === player2_socket_id &&
+      typeof player1_socket_id !== "undefined"
+    ) {
+      //This if condition will go away as game is started only when both start
+      //	console.log('moving position of player1');
+      io.sockets.connected[player1_socket_id].emit("player_move", {
+        message: new_positions,
+      });
+    }
+  });
 });
